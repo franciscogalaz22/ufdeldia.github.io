a7265b867ff5b53d5a7ec2c27ed9f3612feacdd5
ufdeldia <franciscogalaz22@gmail.com>
2016-09-06 22:55:22 -0300

    Add files via upload

--- assets/js/extraeDatos.js
+++ assets/js/extraeDatos.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         var fechaI= document.getElementById('input_from').value;
         var fechaF=document.getElementById('input_to').value;
         
-        google.charts.load('current', {'packages':['table']});
+        
       google.charts.setOnLoadCallback(drawTable);
 
       function drawTable() {
@@ -13,7 +13,7 @@ $(document).ready(function() {
   // Replace the data source URL on next line with your data source URL.
   var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1zBx43V6AhT-fmS6IJPIGkLL502RdznEYrcAtl3V8eq8/edit#gid=261964506', opts);
 
-  // Optional request to return only column C and the sum of column B, grouped by C members.
+  // Query
   query.setQuery("select A,B where A >= date '" + fechaI + "' AND A <= date '" + fechaF + "' ORDER BY A");
 
   // Send the query with a callback function.
@@ -48,37 +48,4 @@ var formatter1 = new google.visualization.DateFormat({pattern: 'dd/MM/yyyy'});
 
 
 
-/*document.getElementById("botonCrearTabla").addEventListener("click",buscarDatos($('#input_from').val,$('#input_to').val));
 
-function buscarDatos(fechaI, fechaF){
-google.charts.load('current', {'packages':['table']});
-      google.charts.setOnLoadCallback(drawTable);
-
-      function drawTable() {
-        var opts = {sendMethod: 'auto'};
-  // Replace the data source URL on next line with your data source URL.
-  var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1zBx43V6AhT-fmS6IJPIGkLL502RdznEYrcAtl3V8eq8/edit#gid=261964506', opts);
-
-  // Optional request to return only column C and the sum of column B, grouped by C members.
-  query.setQuery("select A,B where A >= date '" + fechaI +"' AND date '" + fechaF + "' <= A ORDER BY A");
-
-  // Send the query with a callback function.
-  query.send(handleQueryResponse);
-
-        
-      }
-        
-        function handleQueryResponse(response) {
-if (response.isError()) {
-    alert('Error in query: '+fechaI+fechaF + response.getMessage() + ' ' + response.getDetailedMessage());
-    return;
-  }
-
-  var data = response.getDataTable();
-  var table = new google.visualization.Table(document.getElementById('tablaUF'));
-
-        table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
-}
-    
-}
-*/
---
+++ assets/js/extraeUF.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+   
+        
+        //primero conseguimos la fecha de hoy
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+
+        if(dd<10) {
+            dd='0'+dd
+        } 
+
+        if(mm<10) {
+            mm='0'+mm
+        } 
+
+        today = yyyy + '-' + mm + '-' + dd; 
+    
+         
+        
+        google.charts.load('current', {'packages':['table']});
+      google.charts.setOnLoadCallback(mandarQueryUF);
+
+      function mandarQueryUF() {
+        var opts = {sendMethod: 'auto'};
+  // Replace the data source URL on next line with your data source URL.
+  var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1zBx43V6AhT-fmS6IJPIGkLL502RdznEYrcAtl3V8eq8/edit#gid=261964506', opts);
+
+  // Query
+
+  query.setQuery("select B where A = date '" + today + "'" );
+
+  // Send the query with a callback function.
+  query.send(respuestaQueryUF);
+
+        
+      }
+        
+        function respuestaQueryUF(response) {
+if (response.isError()) {
+    alert('Error in query: '+ response.getMessage() + ' ' + response.getDetailedMessage());
+    return;
+  }
+
+  //guardo resultado de query en data
+  var data = response.getDataTable();
+            
+  //guardo valor de UF en valor, y utilizo un pequeño codigo para formatearlo y que tenga "," en los miles.          
+  var valor = data.getValue(0,0);
+    var parts = valor.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    valor=parts.join(".");
+  
+  //doy formato a la fecha          
+  today= dd + '/' + mm + '/' + yyyy;
+  
+  //escribo el valor de la UF y la fecha en el elemento escogido          
+  document.getElementById('ufDeHoy').innerHTML="" + today + " <strong>" + valor + "</strong>";
+       
+ 
+        
+}
+    
+
+        
+      
+
+});
