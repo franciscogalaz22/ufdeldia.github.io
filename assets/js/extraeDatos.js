ce4a1ea7c274d0ea2bd2efb0fe8ffe2e0ced2603
ufdeldia <franciscogalaz22@gmail.com>
2017-06-13 17:57:26 -0400

    Create extraeDatos.js

--- assets/js/extraeDatos.js
+++ assets/js/extraeDatos.js
@@ -42,12 +42,12 @@ var formatter1 = new google.visualization.DateFormat({pattern: 'dd/MM/yyyy'});
         //table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
             
   if(document.getElementById('graficoElegido').checked && document.getElementById('tablaElegido').checked) {
-    grafico.draw(data,{hAxis: { format:'##.###'} });
+    grafico.draw(data,{vAxis: { format:'##.###'} });
     table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
    }
   else if(document.getElementById('graficoElegido').checked) {
     document.getElementById('tablaUF').innerHTML="";    
-    grafico.draw(data, {hAxis: { format:'##.###'} });
+    grafico.draw(data, {vAxis: { format:'##.###'} });
     
     } 
   else if(document.getElementById('tablaElegido').checked) {
