0939f5af9ed8ee03699672db9815ce2d8c3bf769
ufdeldia <franciscogalaz22@gmail.com>
2017-06-13 13:45:09 -0400

    Create extraeUF.js

--- assets/js/extraeUF.js
+++ assets/js/extraeUF.js
@@ -48,7 +48,7 @@ if (response.isError()) {
             
   //guardo valor de UF en valor, y utilizo un pequeño codigo para formatearlo y que tenga "," en los miles. -->13-06-2017 Ya no editaré el numero, lo dejare la coma como decimal.         
   var valor = data.getValue(0,0);
-  var formatter2 = new google.visualization.NumberFormat({ prefix: '$', decimalSymbol: ',' });
+  var formatter2 = new google.visualization.NumberFormat({ pattern: '$###.###,##' });
     formatter2.format(valor,0);
     //var parts = valor.toString().split(".");
     //parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
